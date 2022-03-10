6a9450857709077612577a58de357e0297680b9f
Balram Khichar <beingbalram@gmail.com>
2022-03-10 16:19:45 +0530

    fix output

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const { exec } = require('child_process');
 const core = require('@actions/core');
 const componentUsage = require('./src/componentUsage');
-
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const ft = core.getInput('flashboard-token');
     core.info(`Starting the analytics....`);
-
-    const usages = await componentUsage({ pattern: /Common\/table\/components\/table/ });
-
-    // grep -l -r "Common/table/components/table" app/modules/
-    exec('grep -l -r "Common/table/components/table" app/modules/', (err, stdout, stderr) => {
-      if (stderr || err) {
-        core.setFailed(stderr || err.message);
-        return;
-      }
-
-      const files = stdout.split('/n');
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${JSON.stringify({files})}`);
-    });
-
+    const usages = await componentUsage({ string: 'Common/table/components/table' });
     core.info(`Usages: ${JSON.stringify(usages)}`);
     core.info('End of analytics!');
   } catch (error) {
--- src/componentUsage.js
+++ src/componentUsage.js
@@ -1,7 +1,23 @@
-const componentUsage = async function ({ pattern }) {
+const _  = require('lodash');
+const core = require('@actions/core');
+const { exec } = require('child_process');
+
+const componentUsage = async function ({ string }) {
+  let files = [];
+  exec(`grep -l -r ${string} app/modules/`, (err, stdout, stderr) => {
+    if (stderr || err) {
+      core.setFailed(stderr || err.message);
+      return;
+    }
+
+    files = _.split(stdout, '/n');
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${JSON.stringify({files})}`);
+  });
+
   return {
-    pattern,
-    occurances: 150,
+    string,
+    occurances: _.size(files),
     occurancesByDirectory: {
       'apps': 50,
       'hris': 20
